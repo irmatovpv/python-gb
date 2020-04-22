eda828c29541de8e23cdd7bf44c4695a7d01f86b
Pavel Irmatov <pavel.irmatov@opensoftdev.ru>
2020-04-22 22:47:10 +0400

    homework-1

---
+++ homework-1.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+print("Hello, {}".format(name))
+
+age = input("Enter your age: ")
+print("This is realy cool to be {}".format(name))
---
+++ homework-3.py
@@ -0,0 +1,7 @@
+n = input("Enter n: ")
+
+one = int(n)
+two = int(n * 2)
+three = int(n * 3)
+
+print(one + two + three)
